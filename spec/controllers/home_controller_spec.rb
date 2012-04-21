e5ae63707ac6615afd6c7be1eaeed09b4a034397
Gareth Aye <django@django.local>
2012-04-21 01:11:41 -0400

    Adds test for logged in user visiting landing page should be redirected

--- spec/controllers/home_controller_spec.rb
+++ spec/controllers/home_controller_spec.rb
@@ -2,9 +2,23 @@ require 'spec_helper'
 
 describe HomeController do
   context 'a logged out user visits home index' do
+    before :each do
+      # TODO(gaye): Logout
+    end
+    
     it 'should be success' do
       get :index
       response.should be_success
+    end
+  end
+  
+  context 'a logged in user visits home index' do
+    before :each do
+      # TODO(gaye): Login
+    end
+    
+    it "should redirect" do
+      response.should be_redirect
     end
   end
 end
