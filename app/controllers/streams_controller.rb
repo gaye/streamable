0874d820e2d51152dcddfa9326d5ef461561ddf6
Gareth Aye <django@django.local>
2012-04-21 11:06:48 -0400

    Makes minor edit to streams controller

--- app/controllers/streams_controller.rb
+++ app/controllers/streams_controller.rb
@@ -23,7 +23,7 @@ class StreamsController < ApplicationController
   
   def create
     session, token = 
-        OpenTok::Helper::create_session_and_generate_moderator_token(current_user, request)
+        OpenTokHelper::create_session_and_generate_moderator_token(current_user, request)
     params[:stream][:publisher_id] = current_user.id
     params[:stream][:opentok_session_id] = session.session_id
     params[:stream][:publisher_token] = token
