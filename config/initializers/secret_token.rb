39b0dbd9d8aadc2a8e1b9ca526861a8efdefafb3
Gareth Aye <django@django.local>
2012-04-20 21:58:11 -0400

    Adds an initial rails skeleton

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Streamable::Application.config.secret_token = 'a057a3c4b33384e1e1cbfde64b0ff9ddc7cbc062969a486177e6b3205fad74aef82e7fef080522414aca70a6519e8fcba239572f9b2933e3a21c32d36f0c9cc4'
