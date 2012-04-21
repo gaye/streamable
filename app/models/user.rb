269b9da4954d2552c7837dc5b7efea537db357e5
Gareth Aye <django@django.local>
2012-04-21 00:11:31 -0400

    Changes User model to have a validation per line

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,12 @@ class User < ActiveRecord::Base
   has_many :subscriptions, :foreign_key => 'subscriber_id'
   has_many :streams_as_publisher, :foreign_key => 'publisher_id', :class_name => 'Stream'
   
-  validates_presence_of :facebook_uid, :first_name, :last_name, :image_url, :token, :token_expiration
+  validates_presence_of :facebook_uid, 
+                        :first_name, 
+                        :last_name, 
+                        :image_url, 
+                        :token, 
+                        :token_expiration
   
   # TODO(gaye): If possible, replace this with a has_many :through
   def streams_as_subscriber
