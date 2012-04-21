0230652df2b265c402b190f01fdc7db02a9425c3
Gareth Aye <django@django.local>
2012-04-21 11:18:55 -0400

    Fleshes out some StreamsController actions

--- app/controllers/streams_controller.rb
+++ app/controllers/streams_controller.rb
@@ -5,23 +5,51 @@
   Date : 04/21/12
 =end
 class StreamsController < ApplicationController
+  # GET /streams
+  # GET /streams.json
   def index
-    # TODO(gaye)
+    @streams = User.all
+    
+    respond_to do |format|
+      format.html
+      format.json { render :json => @streams }
+    end
   end
   
+  # GET /streams/1
+  # GET /streams/1.json
   def show
-    # TODO(gaye)
+    @stream = Stream.find(params[:id])
+    # TODO(gaye): Lookup whether current user is publisher/subscriber
+    
+    respond_to do |format|
+      format.html
+      format.json { render :json => @stream }
+    end
   end
   
+  # GET /streams/new
+  # GET /streams/new.json
   def new
-    # TODO(gaye)
+    # TODO(gaye): Enforce permissions
+    @stream = Stream.new
+
+    respond_to do |format|
+      format.html
+      format.json { render :json => @stream }
+    end
   end
   
+  # GET /streams/1/edit
   def edit
-    # TODO(gaye)
+    # TODO(gaye): Enforce permissions
+    @stream = Stream.find(params[:id])
   end
   
+  # POST /streams
+  # POST /streams.json
   def create
+    # TODO(gaye): Enforce permissions
     session, token = 
         OpenTokHelper::create_session_and_generate_moderator_token(current_user, request)
     params[:stream][:publisher_id] = current_user.id
@@ -29,13 +57,45 @@ class StreamsController < ApplicationController
     params[:stream][:publisher_token] = token
     
     @stream = Stream.new(params[:stream])
+    
+    respond_to do |format|
+      if @stream.save
+        format.html { redirect_to @stream, :notice => "Great! We'll see you at #{@stream.when}!" }
+        format.json { render :json => @stream, :status => :created, :location => @stream }
+      else
+        format.html { render :action => 'new' }
+        format.json { render :json => @stream.errors, :status => :unprocessable_entity }
+      end
+    end
   end
   
+  # PUT /streams/1
+  # PUT /streams/1.json
   def update
-    # TODO(gaye)
+    # TODO(gaye): Enforce permissions
+    @stream = Stream.find(params[:id])
+
+    respond_to do |format|
+      if @stream.update_attributes(params[:stream])
+        format.html { redirect_to @stream, :notice => "Okay got it! We'll see you at #{@stream.when}!" }
+        format.json { head :no_content }
+      else
+        format.html { render :action => 'edit' }
+        format.json { render :json => @stream.errors, :status => :unprocessable_entity }
+      end
+    end
   end
   
+  # DELETE /streams/1
+  # DELETE /streams/1.json
   def destroy
-    # TODO(gaye)
+    # TODO(gaye): Enforce permissions
+    @stream = Stream.find(params[:id])
+    @stream.destroy
+
+    respond_to do |format|
+      format.html { redirect_to streams_url }
+      format.json { head :no_content }
+    end
   end
 end
