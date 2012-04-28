26bbd295c6f843ca219d9024247f07af5f493a6c
Gareth Aye <django@django.local>
2012-04-27 21:42:09 -0400

    Removes delayed job for the time being

--- lib/delayed_job/send_to_amazon_job.rb
+++
@@ -1,5 +0,0 @@
-class SendToAmazonJob < Struct.new(:stream_id)
-  def perform
-    Stream.find(stream_id).async_send_to_s3
-  end
-end
--- spec/lib/send_to_amazon_job_spec.rb
+++
@@ -1,9 +0,0 @@
-require 'spec_helper'
-
-=begin
-  Author : Gareth Aye (gareth@streamable.tv)
-  Date : 04/21/12
-=end
-describe SendToAmazonJob do
-  # TODO(gaye)
-end
