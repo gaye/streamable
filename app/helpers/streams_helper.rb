f16c59b3daa93b2f218c7b7d4cd466e6eb6c8f51
Armaan Sarkar <armaansarkar@gmail.com>
2012-04-28 23:47:41 -0400

    fixes truncate helper method

--- app/helpers/streams_helper.rb
+++ app/helpers/streams_helper.rb
@@ -2,6 +2,6 @@ module StreamsHelper
   def truncate_to_words(string, n)
     shortened = truncate(string, :length => n, :omission => '')
     last_space = shortened.rindex(" ")
-    "#{last_space ? shortened : shortened.slice(0..last_space - 1)}..."
+    "#{last_space ? shortened.slice(0..last_space - 1) : shortened}..."
   end
 end
