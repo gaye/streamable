6b29850fb996c3f2f6eab4d5115320c0bdfa391e
Armaan Sarkar <armaansarkar@gmail.com>
2012-04-28 23:40:15 -0400

    fixes merge issues + stylistic fixes

--- app/helpers/streams_helper.rb
+++ app/helpers/streams_helper.rb
@@ -2,7 +2,6 @@ module StreamsHelper
   def truncate_to_words(string, n)
     shortened = truncate(string, :length => n, :omission => '')
     last_space = shortened.rindex(" ")
-    puts "last space: #{last_space}"
-    last_space == nil ? "#{shortened}..." : "#{shortened.slice(0..last_space-1)}..."
+    "#{last_space ? shortened : shortened.slice(0..last_space - 1)}..."
   end
 end
